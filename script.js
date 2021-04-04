4c9f54eda1de2a9cbbc8577a578a50be111024f7
Ruslanbelit <79848422+Ruslanbelit@users.noreply.github.com>
2021-04-04 15:31:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var Tc = prompt("Ввыедите количество градусов по Цельсию");
+var Tf = (9 / 5) * Tc + 32;
+alert("Градусов по фаренгейту = " + Tf)
+
+
+
+var name = "Василий";
+alert("name = " + name);
+var admin = name;
+alert("admin = " + admin);
+
+/* Это как вариант одного из возможностей копирования
+var name = prompt("Введите имя");
+var admin = name;
+alert("admin = " + admin);*/
